ad6032d64a83542b238da1d7cca191831eba739b
Artemiy Orlov <artemiyo.dev@gmail.com>
2021-08-08 22:25:35 +0300

    dll - pop method

--- dataStructures/doubly-linked-list/index.js
+++ dataStructures/doubly-linked-list/index.js
@@ -26,4 +26,19 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
